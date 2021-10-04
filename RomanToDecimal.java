38c5dc1e9a97b19436e020824540a8df109eb77e
23morrisc <57642421+23morrisc@users.noreply.github.com>
2021-10-03 22:49:28 -0400

    Add files via upload
    Commented romanToDecimal Class

--- RomanToDecimal.java
+++ RomanToDecimal.java
@@ -1,10 +1,23 @@
+/**
+ * converts roman numerals to decimals
+ * @version 10/3/2021
+ * @author 23morrisc
+ */
+
 public class RomanToDecimal {
+
+    /**
+     * calculates value in decimal
+     * @param roman
+     */
+
     public static int romanToDecimal(String roman) {
         int decimal = 0;
         for(int i = 0; i < roman.length(); i++) {
             //System.out.println(roman.length());
             //System.out.println("DEBUG: " + roman.substring(i, i+1).toUpperCase());
 
+            //finds the value of the RN by looking at each character and assessing its value in decimal
             if (roman.substring(i, i + 1).toUpperCase().equals("I")) {
                 decimal += 1;
             } else if (roman.substring(i, i + 1).toUpperCase().equals("V")) {
@@ -24,6 +37,7 @@ public class RomanToDecimal {
             }
         }
 
+            //Finds the reductions within the RN and subtracts the amount necessary to compensate for original error
             if (roman.toUpperCase().indexOf("IV") != -1)
                 decimal -=2;
             if (roman.toUpperCase().indexOf("IX") != -1)
@@ -40,10 +54,17 @@ public class RomanToDecimal {
         return decimal;
     }
 
+    /**
+     * runs the romanToDecimal method for all args
+     * @param args
+     */
+
     public static void main(String[] args) {
         for(String temp : args) {
             int val = romanToDecimal(temp);
-            //System.out.println(val);
+            //DEBUGGING: System.out.println(val);
+
+            //Checks for validity in the returned decimal value and prints the result
             if(val == -1) {
                 System.out.println("Input: " + temp + " => output: invalid");
             }
